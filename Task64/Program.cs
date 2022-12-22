bcd826e11d73be9ab72ddbc7e44a66bde466b75c
a.gazalov <gazalov@list.ru>
2022-12-22 09:45:56 +0300

    Task 64 Showss list  of natural numbers from N till 1

---
+++ Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+void NumbersLineReverse(int firstNumber, int numberN)
+{
+    if (firstNumber <= numberN)
+    {
+        NumbersLineReverse(firstNumber + 1, numberN);
+        Console.Write(firstNumber + " ");
+    }
+}
+
+int NumberEntry(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int firstNumber = 1;
+
+int numberN = NumberEntry("Введите число N: ");
+
+NumbersLineReverse(firstNumber, numberN);
