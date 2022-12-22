03d7f0ad52f24a0ec6090a48979df939cfa5c590
a.gazalov <gazalov@list.ru>
2022-12-22 09:50:46 +0300

    Task 66 Shows the sum of natural elements from M till N

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int NumbersLineReverse(int numberM, int numberN)
+{
+    if (numberM == numberN) return numberM;
+    return NumbersLineReverse(numberM + 1, numberN) + numberM;
+}
+
+int NumberEntry(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int numberM = NumberEntry("Введите число M: ");
+int numberN = NumberEntry("Введите число N: ");
+
+Console.WriteLine($"Сумма натуральных элементов от {numberM} до {numberN} равна {NumbersLineReverse(numberM, numberN)}.");
